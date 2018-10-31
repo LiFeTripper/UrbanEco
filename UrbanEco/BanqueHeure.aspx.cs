94300cb10794dec7c00ddf66b31b21aa2671eeb1
unknown <friendise@live.ca>
2018-10-31 11:31:04 -0400

    Debut Banque Heure

---
+++ UrbanEco/BanqueHeure.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace UrbanEco
+{
+    public partial class BanqueHeure : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btn_modifBH_Click(object sender, EventArgs e)
+        {
+            if(ddl_empBH.Text != "")
+            {
+                tb_BH.Enabled = true;
+            }
+        }
+    }
+}
---
+++ UrbanEco/BanqueHeureEmp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UrbanEco
+{
+    public class BanqueHeureEmp
+    {
+        int idEmp;
+        int heureBanque;
+        int heureVancance;
+        int heureCP;
+        int heure
+    }
+}
