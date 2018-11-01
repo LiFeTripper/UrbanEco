fd2d5091e44ff3b972dc0e51db9b49236c8639a4
Mathieu Rioux <mathieurioux89@Gmail.com>
2018-11-01 11:24:55 -0400

    Mathieu, page projets

--- UrbanEco/Projets.aspx
+++ UrbanEco/Projets.aspx
@@ -1,5 +1,126 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Layout.Master" AutoEventWireup="true" CodeBehind="Projets.aspx.cs" Inherits="UrbanEco.Projets" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+    
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="BodyPlaceHolder" runat="server">
+    
+    <form runat="server">
+        <%--TITRE DESCRIPTION RESPONSABLE--%>
+        <table>
+            <%--TITRE--%>
+            <tr>
+                <th>
+                    <h5 style="padding-right: 200px;">Titre</h5>
+                </th>
+            </tr>
+            <tr>
+                <td>
+                    <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+            <%--DESCRIPTION--%>
+            <tr>
+                <th>
+                    <h5 style="padding-right: 200px;">Description</h5>
+                </th>
+            </tr>
+            <tr>
+                <td>
+                    <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+            <%--RESPONSABLE--%>
+            <tr>
+                <th>
+                    <h5 style="padding-right: 200px;">Responsable</h5>
+                </th>
+            </tr>
+            <tr>
+                <td>
+                    <asp:TextBox ID="TextBox3" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+        </table>
+
+        <%--HEURES DEBUT ET FIN--%>
+                <table>
+            <%--HEURES ALLOUÉ--%>
+            <tr>
+                <th>
+                    <h5 style="padding-right: 200px;">Heures Allouées</h5>
+                </th>
+            </tr>
+            <tr>
+                <td>
+                    <asp:TextBox ID="TextBox5" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+            <%--DATE DEBUT--%>
+            <tr>
+                <th>
+                    <h5 style="padding-right: 200px;">Date de début</h5>
+                </th>
+            </tr>
+            <tr>
+                <td>
+                    <asp:Calendar ID="Calendar1" runat="server"></asp:Calendar>
+                </td>
+            </tr>
+            <%--DATE FIN--%>
+            <tr>
+                <th>
+                    <h5 style="padding-right: 200px;">Date de fin</h5>
+                </th>
+            </tr>
+            <tr>
+                <td>
+                    <asp:Calendar ID="Calendar2" runat="server"></asp:Calendar>
+                </td>
+            </tr>
+        </table>
+
+         <%--CATEGORIES ARCHIVE ET EMPLOYÉS--%>
+                <table>
+            <%--CATÉGORIE--%>
+            <tr>
+                <th>
+                    <h5 style="padding-right: 200px;">Catégories</h5>
+                </th>
+            </tr>
+            <tr>
+                <td>
+                    <asp:ListBox ID="ListBox1" runat="server" DataSourceID="LinqCategorie" DataTextField="titre" DataValueField="titre"></asp:ListBox>
+                <asp:LinqDataSource ID="LinqCategorie" runat="server" ContextTypeName="UrbanEco.CoecoDataContext" EntityTypeName="" Select="new (titre)" TableName="tbl_ProjetCat">
+                </asp:LinqDataSource>
+                <asp:LinqDataSource ID="LinqProjet" runat="server" ContextTypeName="UrbanEco.CoecoDataContext" EntityTypeName="" OrderBy="idCat" Select="new (titre)" TableName="tbl_ProjetCat">
+                </asp:LinqDataSource>
+                </td>
+            </tr>
+            <%--ARCHIVE--%>
+            <tr>
+                <th>
+                    <h5 style="padding-right: 200px;">Archive</h5>
+                </th>
+            </tr>
+            <tr>
+                <td>
+                    <asp:CheckBox ID="CheckBox1" runat="server" />
+                </td>
+            </tr>
+            <%--EMPLOYÉS--%>
+            <tr>
+                <th>
+                    <h5 style="padding-right: 200px;">Employés associés</h5>
+                </th>
+            </tr>
+            <tr>
+                <td>
+                    <asp:ListBox ID="ListBox2" runat="server" DataSourceID="LinqEmploye" DataTextField="prenom" DataValueField="prenom"></asp:ListBox>
+                <asp:LinqDataSource ID="LinqEmploye" runat="server" ContextTypeName="UrbanEco.CoecoDataContext" EntityTypeName="" OrderBy="nom" Select="new (prenom, nom)" TableName="tbl_Employe">
+                </asp:LinqDataSource>
+                </td>
+            </tr>
+        </table>
+
+    </form>
 </asp:Content>
--- UrbanEco/Projets.aspx.designer.cs
+++ UrbanEco/Projets.aspx.designer.cs
@@ -13,15 +13,6 @@ namespace UrbanEco {
     public partial class Projets {
         
         /// <summary>
-        /// Contrôle Label1.
-        /// </summary>
-        /// <remarks>
-        /// Champ généré automatiquement.
-        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label Label1;
-        
-        /// <summary>
         /// Contrôle TextBox1.
         /// </summary>
         /// <remarks>
@@ -29,15 +20,6 @@ namespace UrbanEco {
         /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
         /// </remarks>
         protected global::System.Web.UI.WebControls.TextBox TextBox1;
-        
-        /// <summary>
-        /// Contrôle Label2.
-        /// </summary>
-        /// <remarks>
-        /// Champ généré automatiquement.
-        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label Label2;
         
         /// <summary>
         /// Contrôle TextBox2.
@@ -49,15 +31,6 @@ namespace UrbanEco {
         protected global::System.Web.UI.WebControls.TextBox TextBox2;
         
         /// <summary>
-        /// Contrôle Label3.
-        /// </summary>
-        /// <remarks>
-        /// Champ généré automatiquement.
-        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label Label3;
-        
-        /// <summary>
         /// Contrôle TextBox3.
         /// </summary>
         /// <remarks>
@@ -65,5 +38,86 @@ namespace UrbanEco {
         /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
         /// </remarks>
         protected global::System.Web.UI.WebControls.TextBox TextBox3;
+        
+        /// <summary>
+        /// Contrôle TextBox5.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox5;
+        
+        /// <summary>
+        /// Contrôle Calendar1.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Calendar Calendar1;
+        
+        /// <summary>
+        /// Contrôle Calendar2.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Calendar Calendar2;
+        
+        /// <summary>
+        /// Contrôle ListBox1.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ListBox ListBox1;
+        
+        /// <summary>
+        /// Contrôle LinqCategorie.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.LinqDataSource LinqCategorie;
+        
+        /// <summary>
+        /// Contrôle LinqProjet.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.LinqDataSource LinqProjet;
+        
+        /// <summary>
+        /// Contrôle CheckBox1.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.CheckBox CheckBox1;
+        
+        /// <summary>
+        /// Contrôle ListBox2.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ListBox ListBox2;
+        
+        /// <summary>
+        /// Contrôle LinqEmploye.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.LinqDataSource LinqEmploye;
     }
 }
