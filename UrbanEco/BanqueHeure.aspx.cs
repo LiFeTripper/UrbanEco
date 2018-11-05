6db1554e438613eb2b4d7b219694091892420edd
unknown <friendise@live.ca>
2018-11-05 14:57:43 -0500

    Test Marc

--- UrbanEco/BanqueHeure.aspx.cs
+++ UrbanEco/BanqueHeure.aspx.cs
@@ -1,14 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Linq;
 
 namespace UrbanEco
 {
     public partial class BanqueHeure : System.Web.UI.Page
     {
+        CoecoDataContext cdc = new CoecoDataContext();
+        
+
         protected void Page_Load(object sender, EventArgs e)
         {
 
@@ -20,6 +23,28 @@ namespace UrbanEco
             {
                 tb_BH.Enabled = true;
             }
+        }
+
+        protected void ddl_empBH_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void load_BHemp(string nomEmp)
+        {
+            string[] nomEmpArray = nomEmp.Split(',');
+
+            var id = from tbl in cdc.tbl_Employe
+                     where tbl.prenom == nomEmpArray[0]
+                     where tbl.nom == nomEmpArray[1]
+                     select tbl.idEmploye;
+
+            id = id;
+            //var querry = from tbl2 in cdc.tbl_BanqueHeure
+            //             where tbl.idEmploye == id.Select<>;
+            //             orderby cust.Name ascending
+            //             select tbl.;
+            tb_nbHeureBanque.Text = 
         }
     }
 }
