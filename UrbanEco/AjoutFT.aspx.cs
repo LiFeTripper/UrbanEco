79a75b444a552b16bfe389659d78a94b431473e8
unknown <friendise@live.ca>
2018-11-17 16:57:57 -0500

    Push de la fds

---
+++ UrbanEco/AjoutFT.aspx.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace UrbanEco
+{
+    public partial class AjoutFT : System.Web.UI.Page
+    {
+        CoecoDataContext context = new CoecoDataContext();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!IsPostBack)
+            {
+
+
+                //Query les projet
+                var queryProjet = from tbl in context.tbl_Projet
+                                  orderby tbl.titre
+                                  select tbl;
+
+                tbx_projet.DataSource = queryProjet;
+                tbx_projet.DataBind();
+
+                //Insérer un text pour sélectionne rle projet au début
+                tbx_projet.Items.Insert(0, "Veuillez sélectionner le projet");
+                tbx_projet.SelectedIndex = 0;
+            }
+            
+        }
+
+        protected void tbx_projet_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            //Reset les catégories
+            if (!tbx_categorie.Enabled)
+                tbx_categorie.Enabled = true;
+
+
+            tbx_categorie.DataSource = null;
+            tbx_categorie.DataSourceID = null;
+
+            //Aucun projet choisi
+            if (tbx_projet.SelectedIndex == 0)
+            {
+                tbx_categorie.Enabled = false;
+                tbx_categorie.Items.Clear();
+                tbx_categorie.Items.Add("-----");
+
+                return;
+            }
+
+            //Obtenir les catégorie
+            CoecoDataContext context = new CoecoDataContext();
+            int projectID = int.Parse(tbx_projet.Items[tbx_projet.SelectedIndex].Value);
+
+            var query = from tbl in context.tbl_ProjetCat
+                        where tbl.idProjet == projectID
+                        select tbl;
+
+
+            tbx_categorie.DataSource = query;
+            tbx_categorie.DataBind();
+        }
+
+        protected void Btn_Enreg_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ UrbanEco/AjoutFT.aspx.designer.cs
@@ -0,0 +1,87 @@
+﻿//------------------------------------------------------------------------------
+// <généré automatiquement>
+//     Ce code a été généré par un outil.
+//
+//     Les modifications apportées à ce fichier peuvent provoquer un comportement incorrect et seront perdues si
+//     le code est régénéré.
+// </généré automatiquement>
+//------------------------------------------------------------------------------
+
+namespace UrbanEco {
+    
+    
+    public partial class AjoutFT {
+        
+        /// <summary>
+        /// Contrôle lbl_Top.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lbl_Top;
+        
+        /// <summary>
+        /// Contrôle tbx_projet.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList tbx_projet;
+        
+        /// <summary>
+        /// Contrôle tbx_categorie.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList tbx_categorie;
+        
+        /// <summary>
+        /// Contrôle tbx_nbHeure.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox tbx_nbHeure;
+        
+        /// <summary>
+        /// Contrôle txa_comments.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlTextArea txa_comments;
+        
+        /// <summary>
+        /// Contrôle Calendar.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlInputGenericControl Calendar;
+        
+        /// <summary>
+        /// Contrôle dateFormated.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl dateFormated;
+        
+        /// <summary>
+        /// Contrôle Btn_Enreg.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Btn_Enreg;
+    }
+}
