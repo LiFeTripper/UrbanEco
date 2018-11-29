503f162efabd0ee20057d60035cbe6fc71d79dea
Sypher0rax <42678306+Sypher0rax@users.noreply.github.com>
2018-11-29 10:23:50 -0500

    Changement d'ordi

--- UrbanEco/Employe.aspx.cs
+++ UrbanEco/Employe.aspx.cs
@@ -22,22 +22,6 @@ namespace UrbanEco
 
         }
 
-<<<<<<< HEAD
-=======
-        protected void Btn_Modif_Click(object sender, EventArgs e)
-        {
-            //Référence au bouton dans l'interface
-            Button button = (sender as Button);
-
-
-            //Ramassage du CommandArgument du bouton
-            string commandArgument = button.CommandArgument;
-
-            //Redirige l'adresse vers l'ajout de projet avec le id en projet
-            Response.Redirect("AjoutEmp.aspx?Emp=" + commandArgument);
-        }
-
->>>>>>> d8d14c6679497c88aa06d93edefabc8b39c08e97
         protected void Chkbx_Inactif_CheckedChanged(object sender, EventArgs e)
         {
             showInactive = !showInactive;
