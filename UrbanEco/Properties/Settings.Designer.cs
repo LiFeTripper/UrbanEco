fb10da7e2a1cc4b15a569baabb585a099d4f1df9
Denis Theriault <denis.theriault53@gmail.com>
2019-01-16 09:11:40 -0500

    je ne comprend pas comment sa marche clairement

--- UrbanEco/Properties/Settings.Designer.cs
+++ UrbanEco/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace UrbanEco.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.8.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.5.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -20,17 +20,6 @@ namespace UrbanEco.Properties {
         public static Settings Default {
             get {
                 return defaultInstance;
-            }
-        }
-        
-        [global::System.Configuration.ApplicationScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=(local);Initial Catalog=BD_Coeco;User Id=WEB_USER;Password=1234;Integ" +
-            "rated Security=True")]
-        public string BD_CoecoConnectionString {
-            get {
-                return ((string)(this["BD_CoecoConnectionString"]));
             }
         }
         
