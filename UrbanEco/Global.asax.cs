5c950a89ec118b8a9aeb2c3190c865821869fdf3
maxlajoie99 <max.lajoie99@hotmail.com>
2019-02-06 10:07:33 -0500

    Email job

--- UrbanEco/Global.asax.cs
+++ UrbanEco/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FluentScheduler;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Mail;
@@ -12,7 +13,42 @@ namespace UrbanEco
     {
 
         protected void Application_Start(object sender, EventArgs e) {
+            CoecoDataContext ctx = new CoecoDataContext();
+            tbl_ConfigAdmin config = ctx.tbl_ConfigAdmin.First();
+            Registry registry = new Registry();
 
+            DayOfWeek day = DayOfWeek.Friday;
+            switch (config.jourRappel) {
+                case "Dimanche":
+                    day = DayOfWeek.Sunday;
+                    break;
+                case "Lundi":
+                    day = DayOfWeek.Monday;
+                    break;
+                case "Mardi":
+                    day = DayOfWeek.Tuesday;
+                    break;
+                case "Mercredi":
+                    day = DayOfWeek.Wednesday;
+                    break;
+                case "Jeudi":
+                    day = DayOfWeek.Thursday;
+                    break;
+                case "Vendredi":
+                    day = DayOfWeek.Friday;
+                    break;
+                case "Samedi":
+                    day = DayOfWeek.Saturday;
+                    break;
+                default:
+                    day = DayOfWeek.Monday;
+                    break;
+            }
+            RappelJob rappel = new RappelJob();
+            rappel.Execute();
+            //registry.Schedule(() => rappel).ToRunEvery(0).Weeks().On(DayOfWeek.Wednesday).At(9, 53);
+            //registry.Schedule(() => new RappelJob()).ToRunEvery(0).Weeks().On(day).At(config.heureRappel.Hours, config.heureRappel.Minutes);
+            JobManager.Initialize(registry);
         }
 
         protected void Session_Start(object sender, EventArgs e)
---
+++ UrbanEco/RappelJob.cs
@@ -0,0 +1,86 @@
+﻿using FluentScheduler;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Web;
+
+namespace UrbanEco {
+    public class RappelJob : IJob {
+        public void Execute() {
+            CoecoDataContext ctx = new CoecoDataContext();
+            tbl_ConfigAdmin config = ctx.tbl_ConfigAdmin.First();
+
+            DateTime lastWeek = DateTime.Now.AddDays(-7);
+
+            var heuresLastWeek = from heure in ctx.tbl_FeuilleTemps
+                                    where heure.dateCreation > lastWeek
+                                    select heure;
+
+            var emailBureau = from employe in ctx.tbl_Employe
+                                join heure in heuresLastWeek on employe.idEmploye equals heure.idEmploye into joined
+                                from eh in joined.DefaultIfEmpty()
+                                where eh == null && !(bool)employe.inactif && employe.idTypeEmpl == 1
+                                select employe.email;
+
+            var emailTerrain = from employe in ctx.tbl_Employe
+                               join heure in heuresLastWeek on employe.idEmploye equals heure.idEmploye into joined
+                               from eh in joined.DefaultIfEmpty()
+                               where eh == null && !(bool)employe.inactif && employe.idTypeEmpl == 2
+                               select employe.email;
+
+            /*if ((bool)config.statutRappelBureau) {
+                foreach (string email in emailBureau.ToList()) {
+                    if (!String.IsNullOrEmpty(email)) {
+                        Courriel courriel = new Courriel(email, config);
+                        courriel.Send();
+                    }
+                }
+            }
+
+            if ((bool)config.statutRappelTerrain) {
+                foreach (string email in emailTerrain.ToList()) {
+                    if (!String.IsNullOrEmpty(email)) {
+                        Courriel courriel = new Courriel(email, config);
+                        courriel.Send();
+                    }
+                }
+            }*/
+        }
+    }
+
+    public class Courriel {
+        private List<string> destinataires;
+
+        private tbl_ConfigAdmin config;
+
+        public Courriel(List<string> p_Destinaires, tbl_ConfigAdmin p_config) {
+            destinataires = p_Destinaires;
+            config = p_config;
+        }
+
+        public Courriel(string p_Destinaires, tbl_ConfigAdmin p_config) {
+            destinataires = new List<string>() { p_Destinaires };
+            config = p_config;
+        }
+
+        public void Send() {
+            SmtpClient smtp = new SmtpClient(config.smtpServer);
+            smtp.Port = config.smtpPort;
+            smtp.Credentials = new System.Net.NetworkCredential(config.emailRappel, config.pwdEmailRappel);
+            smtp.EnableSsl = (bool)config.smtpSSL;
+
+            MailMessage mail = new MailMessage();
+            mail.From = new MailAddress(config.emailRappel);
+
+            foreach (string destinataire in destinataires) {
+                mail.To.Add(destinataire);
+            }
+
+            mail.Subject = config.objetRappel;
+            mail.Body = config.contenuRappel;
+
+            smtp.Send(mail);
+        }
+    }
+}
