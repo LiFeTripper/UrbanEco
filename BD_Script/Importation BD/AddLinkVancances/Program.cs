e2938d0f37e445d2a1d25afd26583d8951264ad3
Guillaume Gagnon <guillaume-gagnon@live.ca>
2019-02-06 09:31:00 -0500

    Ajouter BH vide

--- BD_Script/Importation BD/AddLinkVancances/Program.cs
+++ BD_Script/Importation BD/AddLinkVancances/Program.cs
@@ -13,11 +13,14 @@ namespace AddLinkVancances
         static void Main(string[] args)
         {
             //191-194 inclu et 255
+            Console.WriteLine("Cette oppération peut prendre un certain temps...");
             CreateLinks(191);
             CreateLinks(192);
             CreateLinks(193);
             CreateLinks(194);
             CreateLinks(255);
+            AjouterBH();
+            Console.WriteLine("Oppération terminé sans erreur! Appuyer sur une touche pour continuer");
             Console.ReadKey();
         }
 
@@ -50,6 +53,28 @@ namespace AddLinkVancances
                     bd.SubmitChanges();
                 }
                 
+            }
+            bd.Dispose();
+        }
+
+        private static void AjouterBH()
+        {
+            CoEco_BDDataContext bd = new CoEco_BDDataContext();
+
+            List<tbl_Employe> listEmp = bd.tbl_Employe.ToList();
+
+            foreach (tbl_Employe emp in listEmp)
+            {
+                for(int i = 1; i < 6; i++)
+                {
+                    tbl_BanqueHeure bh = new tbl_BanqueHeure();
+                    bh.idEmploye = emp.idEmploye;
+                    bh.idTypeHeure = i;
+                    bh.nbHeure = 0;
+                    bh.nbHeureInitial = 0;
+                    bd.tbl_BanqueHeure.InsertOnSubmit(bh);
+                    bd.SubmitChanges();
+                }
             }
             bd.Dispose();
         }
